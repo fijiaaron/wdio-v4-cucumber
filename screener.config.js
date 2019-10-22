3529d0763c8346f06a3996925820bd0c736fbe6c
nadvolod <nadvolod@gmail.com>
2019-10-22 13:03:11 -0400

    add example of dynamic page load

--- screener.config.js
+++ screener.config.js
@@ -1,7 +1,7 @@
 var Steps = require('screener-runner/src/steps');
 module.exports = {
     // full repository name for your project:
-    projectRepo: 'hilton-poc',
+    projectRepo: 'hilton-poc-3',
   
     // this example assumes Environment Variables listed below exist on your system:
     apiKey: process.env.SCREENER_API_KEY,
@@ -11,28 +11,21 @@ module.exports = {
     // each state consists of a url and a name.
     states: [
       {
-        //This page has dynamic elements that only appear when they are scrolled into view
-        //so we run a script to preload those elements
-        url: 'https://www.ultimateqa.com',
-        name: 'Ultimate QA Home Page',
-        steps: new Steps()
-        .executeScript('window.scrollTo(0,document.body.scrollHeight)')
-        .wait(4000)
-        .snapshot('Loaded')
-        .end()
+        url: 'https://www.hilton.com/en/hotels/laxswhw-homewood-suites-los-angeles-international-airport/',
+        name: 'Hilton Homewood Suites LA'
       },
-      {
-        url: 'https://www.ultimateqa.com/blog',
-        name: 'Ultimate QA Blog Page'
-      },
-      {
-        url: 'https://www.hilton.com',
-        name: 'Hilton Home Page'
-      },
-      {
-        url: 'https://hiltonhonors3.hilton.com/en_US/hh/search/findhotels/index.htm',
-        name: 'Hilton Booking Page'
-      },
+      //If you wanted to dynamically preload the page, you'd do it like this
+      // {
+      //   //This page has dynamic elements that only appear when they are scrolled into view
+      //   //so we run a script to preload those elements
+      //   url: 'https://www.ultimateqa.com',
+      //   name: 'Ultimate QA Home Page',
+      //   steps: new Steps()
+      //   .executeScript('window.scrollTo(0,document.body.scrollHeight)')
+      //   .wait(4000)
+      //   .snapshot('Loaded')
+      //   .end()
+      // }
     ],
     //What are all of the browsers that we want to test against
     //In this case, they are all overriden in the screener.sauce.config
